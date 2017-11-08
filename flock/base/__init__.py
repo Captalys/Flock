144c486cc930eda7db230f99381c89e62c856947
Wanderson Ferreira <iagwanderson@gmail.com>
2017-11-08 19:06:10 -0200

    fixing

--- flock/base/__init__.py
+++ flock/base/__init__.py
@@ -16,14 +16,14 @@ from multiprocessing.pool import Pool
 class NoDaemonProcess(Process):
     """
     :class:`NoDaemonProcess` is that ensure the process' daemon flag is false.
-    
+
     When a process exits, it attempts to terminate all of its daemonic child processes.
-    
+
     It inheritates the Process method from `multiprocessing`
-    Parameters:
-        Process: method inherited from `multiprocessing` package
+
+    :param Process Process: method inherited from `multiprocessing` package
     """
-   
+
     def _get_daemon(self):
         return False
 
@@ -34,10 +34,12 @@ class NoDaemonProcess(Process):
 
 class FlockPool(Pool):
     """
-    :class:`FlockPool` inheritates the Pool method form `multiprocessing` package. It settles the pool size and
+    :class:`FlockPool` inheritates the Pool method from :mod:`multiprocessing` module. It settles the pool size and
     context that will be used to use the function asynchronally.
-        Parameters:
-        Pool: method inherited from `multiprocessing` package
+
+    Parameters
+    ----------
+        Pool: method inherited from :mod:`multiprocessing` module
     """
     Process = NoDaemonProcess
 
@@ -45,10 +47,10 @@ class FlockPool(Pool):
 class BaseMultiProc(object):
     """
     :class:`BaseMultiProc` is a class to apply and manage multiprocessing tasks within fucntions.
-    Parameters:
-        poolSize: amount of resources set to be processed the same time (`default` = 5)\n
-        timeOutError:  degree of tolerance for waiting a process to end (`default` = 50)\n
-        context: way of starting a process (depends os the platform). It can be "spawn", "fork" or "forkserver" (`default`="spawn")
+
+    :param int poolSize: amount of resources set to be processed the same time (`default` = 5)\n
+    :param int timeOutError:  degree of tolerance for waiting a process to end (`default` = 50)\n
+    :param str context: way of starting a process (depends os the platform). It can be "spawn", "fork" or "forkserver" (`default`="spawn")
     """
 
     def __init__(self, poolSize=5, timeOutError=50, context="spawn"):
@@ -60,16 +62,16 @@ class BaseMultiProc(object):
     def getResults(self, result):
         """
         :func:`getResults` append results obtained from functions asynchronally.
-        Parameters: 
-            result: results calculated from the function
+
+        :param list result: results calculated from the function
         """
         self.results.append(result)
 
     def logErrors(self, localQueue):
         """
         :func:`logErrors` takes as argument the queue to be processed and assess if the process has timed-out
-        Parameters:
-            localQueue: list of process to be executed 
+
+        :param list localQueue: list of process to be executed
         """
         for asyncRes in localQueue:
             try:
@@ -82,8 +84,7 @@ class BaseMultiProc(object):
     def dillEncoded(cls, payload):
         """
         :func:`dillEncoded` encodes the already serialized `function` and `argument`
-        Parameter:
-            payload: serialized function and argument
+        :param dict payload: serialized function and argument
         """
         fun, args = dill.loads(payload)
         return fun(*args)
@@ -91,12 +92,12 @@ class BaseMultiProc(object):
     @classmethod
     def customApplyAsync(cls, pool, fun, args, callback):
         """
-        :func:`customApplyAsync` 
-        Parameters:
-            pool: parameters such as poll size and context.
-            fun: a function in which is desired to be ran multiprocessed and asynchronally
-            args: variables to be applied to the function
-            callback: it called, if supplied, when the function is complete
+        :func:`customApplyAsync`
+
+        :param Pool pool: parameters such as poll size and context.
+        :param func fun: a function in which is desired to be ran multiprocessed and asynchronally
+        :param tuple args: variables to be applied to the function
+        :param func callback: it called, if supplied, when the function is complete
         """
         payload = dill.dumps((fun, args))
         return pool.apply_async(cls.dillEncoded, (payload,), callback=callback)
@@ -105,10 +106,10 @@ class BaseMultiProc(object):
         """
         :func:`computeFunc` recieves the function and iterator used in :func:`executeAsync` together with an endpoint for the
         processor. It calculates the results of the function, for each iterator and sets queue of process to be executed.
-        Parameters:
-            childCon: endpoint conection\n
-            function: a function in which is desired to be ran multiprocessed and asynchronally\n
-            iterator: variable in wich the function will be applied
+
+        :param Pipe childCon: endpoint conection\n
+        :param func function: a function in which is desired to be ran multiprocessed and asynchronally\n
+        :param iter iterator: variable in wich the function will be applied
         """
         pool = FlockPool(self.poolSize, context=get_context(self.context))
         localQueue = []
@@ -125,10 +126,10 @@ class BaseMultiProc(object):
     def executeAsync(self, function, iterator):
         """
         Method :func:`executeAsync` executes a function asynchronally, given a set of iterators.
-        Parameters:
-            function: a function in which is desired to be ran multiprocessed and asynchronally\n
-            iterator: variable in wich the function will be applied
-        
+
+        :param func function: a function in which is desired to be ran multiprocessed and asynchronally\n
+        :param iter iterator: variable in wich the function will be applied
+
         Returns the result of the function given a set of arguments of that function.
         """
         parentCon, childCon = Pipe()
@@ -138,20 +139,3 @@ class BaseMultiProc(object):
         res = parentCon.recv()
         parentProcess.join()
         return res
-
-
-# def funcHelper1(a):
-#     from flock.base.database import SqlCon
-#     try:
-#         engine = SqlCon.mysql_engine("varejo")
-#         conn = engine.connect()
-#         b = (a ** 2)
-#         return b
-#     except Exception as e:
-#         raise e
-
-# if __name__ == '__main__':
-#     bp = BaseMultiProc()
-#     iterator = list(range(100))
-#     res = bp.executeAsync(funcHelper1, iterator)
-#     print(res)
--- flock/dataframe/__init__.py
+++ flock/dataframe/__init__.py
@@ -11,60 +11,51 @@ class DataFrameAsync(object):
     @classmethod
     def applyInRows(cls, args):
         """
-        :func:`Applyinrows` method takes a dataframe and the function to be applied at as arguments. Stores every row of the dataframe at a list.
-        Through :func:`executeAsync()`   BaseMultiProc's method, it processes the fuctions at each element of the list (row) in parallel calling different processes and 
-        in the end returns the resulting Dataframe by concating the processed rows.  
+        :func:`Applyinrows` method takes a dataframe and the function to be applied at as arguments.
+        It stores every row of the dataframe in a list list.
+        Through :func:`BaseMultiProc.executeAsync()` method, it processes the fuctions at each element of the list (row) in parallel calling different processes
 
-        func: Function that will be applied at the Dataframe 
+        Returns a Dataframe by concating the processed rows.
 
-        df: Dataframe passed as argument
-
+        :param tuple args: Function that will be applied at the Dataframe
         """
         func, block_df = args
         _df2 = block_df.copy()
         list_rows = []
 
-       
         for i in range(len(_df2)):
             list_rows.append(_df2.iloc[i:i+1])
 
         bp2 = BaseMultiProc()
-        
+
         res2 = bp2.executeAsync(func, list_rows)
         res2 = concat(res2)
         return res2
-
 
     @classmethod
     def apply(cls, dataframe, function, style="row-like",
               chunksize=100, poolSize=5):
         """
-        Apply method receives the dataframe,  the function you want to apply on it, style, chunksize and poolsize.  The first step is the segmentation of the orgininal dataframe in chunks, generating a list of 
-        dataframes called iterator. Then the :func:`executeAsync()` will parallelize the function's operatations on the segmented dataframes and then concatenate the chunks, returning the 
-        excepted result. There two options for the way it will operate, as 'row-like' or 'block-like'
+        The first step is the segmentation of the orginal dataframe in chunks, generating a list of
+        dataframes called iterator.
+        Then the :func:`executeAsync()` will parallelize the function's operatations on the segmented dataframes and then concatenate the chunks, returning the
+        excepted result.
 
-        dataframe: Input Dataframe
+        There two options for the way it will operate, as 'row-like' or 'block-like'
 
-        fuction: Function  to be applied on the dataframe
-
-        chunksize: Defines on how many chunks will the original dataframe be splitted 
-
-        poolSise: Defines number of  pools of processes
-
-        style: if  set as "row-like", the list of dataframes resulted from the chunking process is passed along with 
-        the function to the 'Applyinrows' to the :func:`executeAsync()`. Basically what will happen is that the process on :func:`Applyinrows` will occur for a list of dataframes, in parellel. While, if it is passed as "Block-like",  the 
+        :param DataFrame dataframe: Input Dataframe
+        :param func fuction: Function  to be applied on the dataframe
+        :param int chunksize: Defines on how many chunks will the original dataframe be splitted
+        :param int poolSize: Defines number of  pools of processes
+        :param str style: if  set as "row-like", the list of dataframes resulted from the chunking process is passed along with
+        the function to the 'Applyinrows' to the :func:`executeAsync()`. Basically what will happen is that the process on :func:`ApplyInrows` will occur for a list of dataframes, in parallel. While, if it is passed as "Block-like",  the
         the process is similar to the method :func:`Applyinrows` but will happen for the whole list of dataframes.
-        
-
-
         """
         _df = dataframe.copy()
-        #lista de dfs
         iterator = split_chunks(_df, _type="dataframe", size=chunksize)
 
         bp = BaseMultiProc(poolSize=poolSize)
 
-        #if row-like, 
         if style == "row-like":
             iterator = [(function, el) for el in iterator]
             res = bp.executeAsync(cls.applyInRows, iterator)
@@ -78,11 +69,3 @@ class DataFrameAsync(object):
         res = concat(res)
         res = res.sort_index()
         return res
-        """
-        TESTE
-        """
-
-
-if __name__ == '__main__':
-    dt = DataFrameAsync()
-  
