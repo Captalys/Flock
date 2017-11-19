a8d4d00b022be0810b7c75e7872390c6b2ec778e
wandferreira <31264554+wandferreira@users.noreply.github.com>
2017-11-19 19:09:23 -0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -170,9 +170,9 @@ texinfo_documents = [
 ]
 
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#if not on_rtd:
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
